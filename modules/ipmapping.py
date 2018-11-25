25611b14aca92f71c478a57547d2c6fc14831544
Sidney G Dodson <siddodson@gmail.com>
2018-11-24 19:02:05 -1000

    Add input validator and comments

--- modules/ipmapping.py
+++ modules/ipmapping.py
@@ -1,10 +1,18 @@
 import socket
+import re
+
+# This module method uses socket to get the IP Addresses associated
+# with a domain string
 
 results = []
-def get_ip(name):
-    res = socket.getaddrinfo(name[0], 80)
+def get_ip(domain_name):
+    name = domain_name[0]
+    if(re.match('http://', name)):
+        name = name[7:]
+    elif(re.match('https://', name)):
+        name = name[8:]
+    res = socket.getaddrinfo(name, 80)
     for each in res:
         if(len(each)<6 and len(each)>4):
             results.append(each[4])
-
     return set(results)
